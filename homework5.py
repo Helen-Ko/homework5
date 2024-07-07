d50842ed780c3c3593abdb717535c7c3f1a8d587
Helen-Ko <karamysheva78@yandex.ru>
2024-07-07 19:55:03 +0500

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,6 +1,6 @@
 immutable_var = 1, 2, 'summer', 'cherry', True
 print(immutable_var)
-#immutable_var[3] = 'spring' - изменить значение элемента кортежа ('cherry')  нельзя, потому что оно неизменяемое
+#immutable_var[3] = 'spring' - изменить значение элемента кортежа ('cherry')  нельзя, потому что оно неизменяемое, т.к. кортеж не поддерживает обращение по элементам
 mutable_list = [3, 4, 'apple', 'phone']
 print(mutable_list)
 mutable_list[3] = 'pineapple'
